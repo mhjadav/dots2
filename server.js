aa56cd6f6a017185b52acd2026e9d77df8533f49
Mahipatsinh Jadav <mj@MacBook-Pro.local>
2018-04-01 13:46:16 +0530

    Frist Commit

---
+++ server.js
@@ -0,0 +1,15 @@
+var express = require('express');
+var app = express();
+app.use(express.static('public'));
+app.get('/', function (req, res) {
+   res.sendFile( __dirname + "/" + "index.html" );
+});
+
+app.get('/confirm', function (req, res) {
+    res.sendFile( __dirname + "/" + "confirm.html" );
+ });
+
+var server = app.listen(8081, function () {
+   var host = server.address().address
+   var port = server.address().port
+});
