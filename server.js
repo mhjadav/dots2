db8b45e02cc7f56112146c5f418227114ec2febd
Mahipatsinh Jadav <mj@MacBook-Pro.local>
2018-04-13 22:05:59 +0530

    Folder structure changes

--- server.js
+++ server.js
@@ -1,14 +1,13 @@
 var express = require('express');
 var app = express();
-app.use(express.static('public'));
+
 app.get('/', function (req, res) {
    res.sendFile( __dirname + "/" + "index.html" );
 });
-
-app.get('/confirm', function (req, res) {
-    res.sendFile( __dirname + "/" + "confirm.html" );
- });
-
+app.get('*', function (req, res) {
+    console.log(req.originalUrl);
+   res.sendFile( __dirname + "/" + req.originalUrl );
+});
 var server = app.listen(8000, function () {
    var host = server.address().address
    var port = server.address().port
