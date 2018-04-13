4ba49ac8c1801f7067c9a4c6e053c2a089371199
Mahipatsinh Jadav <mj@MacBook-Pro.local>
2018-04-13 21:50:05 +0530

    Images changes

--- public/js/renderHtml.js
+++ public/js/renderHtml.js
@@ -1,34 +1,89 @@
 $(document).ready(function () {
     renderGrid();
 });
-
-var renderGrid = function(){
+var gridParams = {};
+var renderGrid = function () {
     var maxTDperTR = 15;
-    var maxTR = 16;
+    var maxTR = 15;
 
     var strHtml = "";
     var x = 1;
     var i = 0;
-    for (var r = 1; r < maxTR; r++) {
+    var boxIndex = 6;
+    for (var r = 1; r <= maxTR; r++) {
         strHtml += "<tr>"
         for (var c = 1; c <= maxTDperTR;) {
-            var width = 30;
-            var height = 5;
+            var width = 35;
+            var height = 6;
+            var imageUrl = '/dots/images/clearline.gif';
+            var eventName = "convert(" + i + ");";
+            var addValue = '';
+            var addValueParamsOne = [];
+            var addValueParamsTwo = [];
+            if (r % 2 === 0) {
+                if(c === 1){
+                    addValueParamsOne.push(i+1);
+                    addValueParamsOne.push(2);
+                }else if(c === 15){
+                    boxIndex += 8;
+                    addValueParamsOne.push(i-1);
+                    addValueParamsOne.push(4);
+                } else {
+                    if (c % 2 != 0) {
+                        addValueParamsOne.push(i-1)
+                        addValueParamsOne.push(4);
+                        addValueParamsTwo.push(i+1);
+                        addValueParamsTwo.push(2);
+                    }
+                }
+            } else if(c != 15){
+                if (r === 1){
+                    boxIndex += 2;
+                    addValueParamsOne.push(boxIndex);
+                    addValueParamsOne.push(1);
+                }
+                else if (r === 15) {
+                    boxIndex += 2;
+                    addValueParamsOne.push(boxIndex - 22);
+                    addValueParamsOne.push(8);
+                } else {
+                    boxIndex += 2;
+                    addValueParamsOne.push(boxIndex - 22)
+                    addValueParamsOne.push(8);
+                    addValueParamsTwo.push(boxIndex) 
+                    addValueParamsTwo.push(1);
+                }
+            }
+
             if (r % 2 != 0) {
-                strHtml += '<td align="center"><input type="radio" name="x' + x + '" onclick="convert(' + x + ');" /></td>';
+                strHtml += '<td align="center"><input type="radio" name="x' + x + '" onclick="this.checked=false;" /></td>';
                 x++;
                 c++;
             } else {
                 if (c % 2 === 0) {
                     width = 30;
                     height = 30;
+                    imageUrl = '/dots/images/clear.gif';
+                    eventName = "";
                 } else {
-                    width = 5;
-                    height = 30;
+                    width = 8;
+                    height = 35;
                 }
             }
             if (!(c === 16 && r % 2 != 0)) {
-                strHtml += '<td align="center"><img alt="" name="i' + i + '" src="images/clear.gif" width="' + width + '" height="' + height + '" /></td>'
+                strHtml += '<td align="center"><img alt="" onclick="'
+                // if(addValueParamsOne.length > 0){
+                //     strHtml += 'adValue(' + addValueParamsOne.join() + ');';                    
+                // }
+                // if(addValueParamsTwo.length > 0){
+                //     strHtml += 'adValue(' + addValueParamsTwo.join() + ');';                    
+                // }
+                gridParams[i] = {
+                    addValueParamsTwo: addValueParamsTwo,
+                    addValueParamsOne: addValueParamsOne
+                }
+                strHtml += eventName + '"';
+                strHtml += ' name="i'+ i +'" src="' + imageUrl + '" width="' + width + '" height="' + height + '" /></td>'
                 i++;
             }
             c++;
@@ -36,6 +91,6 @@ var renderGrid = function(){
     }
     strHtml += ' <tr class="spacer"><td style="padding-bottom: 1em;x"></td></tr><tr><td bgcolor="silver" colspan="7"> Computer:<input type="text" name="comp" size="2" value="0" style="width: 50%;text-align: center;" /></td>';
     strHtml += '<td bgcolor="silver" colspan="5">Player:<input type="text" name="plyr" size="2" value="0" style="width: 50%;text-align: center;"/></td>';
-    strHtml += '<td bgcolor="silver" colspan="3"><input type="button" value="Reset" onclick="renderGrid()" /></td></tr>';
+    strHtml += '<td bgcolor="silver" colspan="3"><input type="button" value="Reset" onclick="renderGrid();init();" /></td></tr>';
     $("#dotsTable").html(strHtml);
 }
